78befac897e4feebb83795ee8b279c97e70ed9fd
Paul Silvis <paulsilvis@gmail.com>
2012-11-25 21:11:10 -0800

    configuring root path

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
+http_path = "/Portfolio/"
 http_images_path = "/Portfolio/images"
-http_fonts_path = "/fonts"
-css_dir = "/Portfolio/stylesheets"
+http_fonts_path = "/Portfolio/fonts"
+css_dir = "public/Portfolio/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
